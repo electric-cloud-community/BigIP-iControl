dd5e2ac313a513a6e727c55cee10786c7b845843
Valentyn Solomko <Valentyn.Solomko@gmail.com>
2020-10-05 13:52:36 +0300

    Improve checkConnection

--- dsl/properties/groovy/lib/BigIPiControl.groovy
+++ dsl/properties/groovy/lib/BigIPiControl.groovy
@@ -1,9 +1,8 @@
 import com.cloudbees.flowpdf.*
 import iControl.CommonAddressPort
 import iControl.CommonEnabledState
-import iControl.CommonStatisticType
 import iControl.Interfaces
-import iControl.LocalLBPoolBindingStub
+
 
 /**
  * BigIPiControl
@@ -21,13 +20,6 @@ class BigIPiControl extends FlowPlugin {
         ]
     }
 
-    // Static variables that explain the n^2 complex relationship
-    // between nodes, pools, and pool members. General names were
-    // given to apply to enabled/disabled, available/unavailable, existant/nonexistant
-    public static final def NEGATIVE = 0              // Disabled/Unavailable/Nonexistant Results
-    public static final def POSITIVE_AND_NEGATIVE = 1 // Mixed Results
-    public static final def POSITIVE = 2              // Enabled/Available/Existant Results
-
     @Lazy
     Interfaces interfaces = {
         def config = context.getConfigValues()
@@ -42,16 +34,25 @@ class BigIPiControl extends FlowPlugin {
     /** This is a special method for checking connection during configuration creation
      */
     def checkConnection(StepParameters p, StepResult sr) {
-        // Use this pre-defined method to check connection parameters
         try {
-            // Put some checks here
             String version = interfaces.getSystemSystemInfo().get_version()
             println version
         } catch (Throwable e) {
-            // Set this property to show the error in the UI
-            sr.setOutcomeProperty("/myJob/configError", e.message)
+            def errMsg = "Connection check for Big-IP failed: " + e.message
+            def suggestions = '''Reasons could be due to one or more of the following. Please ensure they are correct and try again:
+1. Are your 'Hostname' and 'Port' correct?
+2. Are your credentials correct?
+'''
+
+            sr.setOutcomeProperty('/myJobStep/outcome', 'error')
+            sr.setOutcomeProperty('/myJobStep/summary', 'Error: ' + suggestions + "\n\n" + errMsg)
+            sr.setOutcomeProperty('/myJob/configError', suggestions + "\n\n" + errMsg)
             sr.apply()
-            throw e
+
+            log.logErrorDiag("Create Configuration failed.\n\n" + errMsg)
+            log.logInfoDiag(suggestions)
+
+            System.exit(-1)
         }
     }
 
@@ -161,44 +162,6 @@ class BigIPiControl extends FlowPlugin {
     //~ }
     //~ }
 
-
-    // Checks the number of connections to a pool member
-    // Used to confirm that a pool member is disabled
-    private int getPoolMemberActiveConnections(String[] poolNames, CommonAddressPort[][] commonAddressPort) {
-        def Long result = -1
-
-        // Retrieve the statistics for each pool
-
-        def pools = interfaces
-            .getLocalLBPool()
-            .get_member_statistics(poolNames, commonAddressPort)
-
-        for (pool in pools) {
-            // Retrieve the statistics for each pool member
-            for (memberStatistics in pool.statistics) {
-                // Iterate through each pool member's statistic
-                for (statistic in memberStatistics.statistics) {
-                    if (statistic.type == CommonStatisticType.STATISTIC_SERVER_SIDE_CURRENT_CONNECTIONS) {
-                        if (result == -1) {
-                            result = statistic.value.low
-                        } else {
-                            result = statistic.value.low + result
-                        }
-                        break
-                    }
-                }
-            }
-        }
-
-        if (result < 0) {
-            throw new Exception("Could not locate node(s)")
-        }
-
-        println "Active connection count is ${result}"
-
-        return result
-    }
-
 /**
  * changeBalancingPoolMemberStatus - Change Balancing Pool Member Status/Change Balancing Pool Member Status
  * Add your code into this method and it will be called when the step runs
@@ -213,30 +176,26 @@ class BigIPiControl extends FlowPlugin {
     def changeBalancingPoolMemberStatus(StepParameters p, StepResult sr) {
         ChangeBalancingPoolMemberStatusParameters sp = ChangeBalancingPoolMemberStatusParameters.initParameters(p)
 
-        Long sleepInterval = Long.parseLong(sp.sleepInterval)
-        boolean doWait = sp.doWait
-
         String partitionName = sp.partitionName.replaceAll('\\\\', '/')
         if (!partitionName) {
-            throw new Exception("Partition Name is empty")
+            throw new RuntimeException("Partition Name is empty")
         }
 
-        String commonPrefix = partitionName
-        if (!commonPrefix.startsWith('/')) {
-            commonPrefix = '/' + commonPrefix
+        if (!partitionName.startsWith('/')) {
+            partitionName = '/' + partitionName
         } else {
-            commonPrefix.replaceFirst('^[/]{2,}', '/')
+            partitionName.replaceFirst('^[/]{2,}', '/')
         }
 
-        if (commonPrefix[-1] != '/') {
-            commonPrefix = commonPrefix + '/'
+        if (!partitionName.matches('/$')) {
+            partitionName = partitionName + '/'
         } else {
-            commonPrefix.replaceFirst('[/]{2,}$', '/')
+            partitionName.replaceFirst('[/]{2,}$', '/')
         }
 
-        String poolName = commonPrefix + sp.poolName.replaceAll("\\s+", "")
+        String poolName = partitionName + sp.poolName.replaceAll("\\s+", "")
         if (!poolName) {
-            throw new Exception("Partition Name is empty")
+            throw new RuntimeException("Pool Name is empty")
         }
 
         String[] poolNames = [poolName]
@@ -244,75 +203,51 @@ class BigIPiControl extends FlowPlugin {
         String[] membersNames = sp.membersNames
             .split(',')
             .join("\n")
+            .split(';')
+            .join("\n")
             .split(/[\r\n]+/)
 
-        if (membersNames.size() == 0) {
-            throw new Exception("Members list is empty")
-        }
-
-        def commonAddressPort = [][]
         def plainAddressPort = []
         for (member in membersNames) {
             def mem = member.trim()
             if (mem != "") {
                 String[] addressPort = mem.split(":")
                 plainAddressPort.add(new CommonAddressPort(
-                    commonPrefix + addressPort[0],
+                    partitionName + addressPort[0],
                     Long.parseLong(addressPort[1])
                 ))
             }
         }
 
-        commonAddressPort.add(plainAddressPort)
+        if (plainAddressPort.size() == 0) {
+            throw new RuntimeException("Members list is empty")
+        }
 
         def pool = interfaces.getLocalLBPool()
 
+        CommonEnabledState state = (sp.setStatus == "enabled") ? CommonEnabledState.STATE_ENABLED : CommonEnabledState.STATE_DISABLED
         def plainEnabledState = []
-        def commonEnabledState = [][]
-        commonEnabledState.add(plainEnabledState)
+        membersNames.each {
+            plainEnabledState.add(state)
+        }
 
         String outcome = "success"
-        String summary = ""
+        String summary
         String data = ""
         try {
-            if ((sp.setStatus == "enabled") || (sp.setStatus == "force_on")) {
-                membersNames.each {
-                    plainEnabledState.add(CommonEnabledState.STATE_ENABLED)
-                }
-
-                pool.set_member_session_enabled_state(poolNames, commonAddressPort as CommonAddressPort[][], commonEnabledState as CommonEnabledState[][])
-                if (sp.setStatus == "force_on") {
-                    pool.set_member_monitor_state(poolNames, commonAddressPort as CommonAddressPort[][], commonEnabledState as CommonEnabledState[][])
-
-                    summary = "Successfully forced offline"
-                } else {
-                    summary = "Successfully enabled"
-                }
+            if (sp.setStatus == "enabled") {
+                pool.set_member_session_enabled_state(poolNames, [plainAddressPort] as CommonAddressPort[][], [plainEnabledState] as CommonEnabledState[][])
+                summary = "Successfully enabled"
             } else if ((sp.setStatus == "disabled") || (sp.setStatus == "force_off")) {
-                membersNames.each {
-                    plainEnabledState.add(CommonEnabledState.STATE_DISABLED)
-                }
-
-                pool.set_member_session_enabled_state(poolNames, commonAddressPort as CommonAddressPort[][], commonEnabledState as CommonEnabledState[][])
+                pool.set_member_session_enabled_state(poolNames, [plainAddressPort] as CommonAddressPort[][], [plainEnabledState] as CommonEnabledState[][])
                 if (sp.setStatus == "force_off") {
-                    pool.set_member_monitor_state(poolNames, commonAddressPort as CommonAddressPort[][], commonEnabledState as CommonEnabledState[][])
-
+                    pool.set_member_monitor_state(poolNames, [plainAddressPort] as CommonAddressPort[][], [plainEnabledState] as CommonEnabledState[][])
                     summary = "Successfully forced offline"
                 } else {
-                    if (doWait) {
-                        def currentConnections = getPoolMemberActiveConnections(poolNames, commonAddressPort as CommonAddressPort[][])
-                        while (currentConnections > 0) {
-                            println("Waiting for ${currentConnections} to close, sleep for ${sleepInterval} second(s)")
-                            sleep(sleepInterval * 1000L, { println("Waiting aborted!"); return true })
-                            currentConnections = getPoolMemberActiveConnections(poolNames, commonAddressPort as CommonAddressPort[][])
-                        }
-                        println("All connections closed, done waiting.")
-                    }
-
                     summary = "Successfully disabled"
                 }
             } else {
-                throw new Exception("Unknown desired status")
+                throw new RuntimeException("Unknown desired status")
             }
         } catch (Throwable e) {
             e.printStackTrace()
@@ -323,7 +258,7 @@ class BigIPiControl extends FlowPlugin {
         }
 
 
-        String resultPropertySheet = sp.resultPropertySheet;
+        String resultPropertySheet = sp.resultPropertySheet
         if (resultPropertySheet == "") {
             resultPropertySheet = "/myJob/poolMemberStatus"
             log.info("Assumed result property sheet: " + resultPropertySheet)
